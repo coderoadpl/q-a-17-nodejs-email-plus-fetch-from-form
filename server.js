acdab006cd531eae46450508a309df2790dab83f
Mateusz Choma <chomamateusz@gmail.com>
2020-12-30 20:23:29 +0100

    Responding with query parameter

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Hello NodeJS!!!')
+const express = require('express')
+
+const app = express()
+const port = 3333
+
+app.get('/', (req, res) => {
+  res.send(req.query.message)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
