c89838eba2e5b45d8a124b786af03f6cec2058c1
Mateusz Choma <chomamateusz@gmail.com>
2020-12-30 20:36:20 +0100

    Using env variables

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 
 const app = express()
 const port = 3333
+
+nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // upgrade later with STARTTLS
+  auth: {
+    user: "coderoadmailer@gmail.com",
+    pass: "password"
+  }
+})
 
 const SUBJECT = 'Mail from CodeRoad form!'
 
@@ -11,7 +23,7 @@ app.get('/', (req, res) => {
   if(!message) return res.status(400).send()
   if(!email) return res.status(400).send()
 
-  res.send( email + ' ' + SUBJECT +  ' ' + message )
+  res.send( email + ' ' + SUBJECT +  ' ' + message + ' ' + process.env.SMTP_PASSWORD )
 })
 
 app.listen(port, () => {
